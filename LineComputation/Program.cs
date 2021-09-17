ed71feaddc436fefa7c36006692cecba71b200fd
nibedita260 <deepi.nibedita260.com>
2021-09-17 12:36:56 +0530

    [Nibedita]Add:CheckComparision LineComparision Feature Implemented

--- LineComputation/Program.cs
+++ LineComputation/Program.cs
@@ -9,14 +9,18 @@ namespace LineComputation
             LineComputation lineComputation = new LineComputation();
             double length1=lineComputation.LengthOfLine(30.0, 20.0, 50.0, 40.0);
             double length2= lineComputation.LengthOfLine(40.0, 25.0, 55.0, 45.0);
-            Console.WriteLine("length of line 1:" + length1 + "and length of line2" + length2);
-            if (length1.Equals(length2))
+            Console.WriteLine("length of line1:" + length1 + "and length of line2:" + length2);
+            if (length1.CompareTo(length2)==0)
             {
-                Console.WriteLine("Lines are Equal");
+                Console.WriteLine("line1: "+length1 + "is equal to line2:"+ length2);
             }
-            else
+            if (length1.CompareTo(length2)> 0)
             {
-                Console.WriteLine("Lines are not Equal");
+                Console.WriteLine("line1: "+length1 + "is greater than line2:" + length2);
+            }
+            if (length1.CompareTo(length2) == 0)
+            {
+                Console.WriteLine("line1: "+length1 + "is less than line2:" + length2);
             }
         }
     }
