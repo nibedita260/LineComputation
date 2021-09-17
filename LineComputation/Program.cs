0aceb13040759655db691315d3540913e072f7c6
nibedita260 <deepi.nibedita260.com>
2021-09-17 12:28:50 +0530

    [Nibedita]Add:CcheckEquality LineComparision Feature Implemented

--- LineComputation/Program.cs
+++ LineComputation/Program.cs
@@ -7,7 +7,17 @@ namespace LineComputation
         static void Main(string[] args)
         {
             LineComputation lineComputation = new LineComputation();
-            Console.WriteLine("The length of line: " + lineComputation.LengthOfLine(30.0, 20.0, 50.0, 40.0));
+            double length1=lineComputation.LengthOfLine(30.0, 20.0, 50.0, 40.0);
+            double length2= lineComputation.LengthOfLine(40.0, 25.0, 55.0, 45.0);
+            Console.WriteLine("length of line 1:" + length1 + "and length of line2" + length2);
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not Equal");
+            }
         }
     }
 }
