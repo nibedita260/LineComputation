1da03e80797009dae50625af990b93163d321e35
nibedita260 <deepi.nibedita260.com>
2021-09-17 12:17:44 +0530

    [Nibedita]Add:CalLength LineComparision Feature Implemented

---
+++ LineComputation/LineComputation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComputation
+{
+    class LineComputation
+    {
+        public double LengthOfLine(double X1, double Y1, double X2, double Y2) // calculates the length of the line
+        {
+            Console.WriteLine("finding the length of line:");
+            double x_total, y_total, total;
+            x_total = X1 - X2;
+            y_total = Y1 - Y2;
+            total = Math.Sqrt((x_total * x_total) + (y_total * y_total));
+            return total;              // length of the Line
+        }
+    }
+}
---
+++ LineComputation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LineComputation lineComputation = new LineComputation();
+            Console.WriteLine("The length of line: " + lineComputation.LengthOfLine(30.0, 20.0, 50.0, 40.0));
+        }
+    }
+}
